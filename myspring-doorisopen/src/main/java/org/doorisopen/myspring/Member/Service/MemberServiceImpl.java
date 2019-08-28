3215675f96c77948343ccaf34279c7753b09e676
doorisopen <audauddl2@gmail.com>
2019-08-28 16:01:52 +0900

    Exception(DataNotFound)

--- myspring-doorisopen/src/main/java/org/doorisopen/myspring/Member/Service/MemberServiceImpl.java
+++ myspring-doorisopen/src/main/java/org/doorisopen/myspring/Member/Service/MemberServiceImpl.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.doorisopen.myspring.Member.Domain.MemberVO;
 import org.doorisopen.myspring.Member.Persistence.MemberDAO;
+import org.doorisopen.myspring.exception.DataNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +16,7 @@ public class MemberServiceImpl implements MemberService {
 	private MemberDAO memberDAO;
 	
 	public MemberVO readMember(String id) throws Exception {
+		if(memberDAO.read(id) ==  null) throw new DataNotFoundException(id);
 		return memberDAO.read(id);
 	}
 	
